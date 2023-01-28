680700d96208a6c5ad7f4aca987056047bc8c944
Aslin-R <aslinjoelr123@gmail.com>
2023-01-28 12:11:34 +0530

    Modified view.py and urls.py

--- django2/djangocoursera/myapp/views.py
+++ django2/djangocoursera/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    content='<html><body><h1>Welcome to Django Framework</html></body></h1>'
+    return HttpResponse(content)
+
+
+
+
+
