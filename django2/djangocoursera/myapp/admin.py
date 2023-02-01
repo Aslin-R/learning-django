a89c04b44cadc735b6469291e38ffd92c5dce916
Aslin-R <aslinjoelr123@gmail.com>
2023-02-01 21:10:27 +0530

    Completed 2nd lab in week3

--- django2/djangocoursera/myapp/admin.py
+++ django2/djangocoursera/myapp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Menu
 from .models import MenuCategory
+from .models import DrinksCategory
+from .models import Drinks
 # Register your models here.
 
 
 admin.site.register(Menu)
 admin.site.register(MenuCategory)
+
+admin.site.register(DrinksCategory)
+admin.site.register(Drinks)
 
--- django2/djangocoursera/myapp/models.py
+++ django2/djangocoursera/myapp/models.py
@@ -7,3 +7,14 @@ class Menu(models.Model):
     menu_itm=models.CharField(max_length=100)
     price=models.IntegerField(null=False)
     category_id=models.ForeignKey(MenuCategory,on_delete=models.PROTECT,default=None,related_name='category_name')
+
+class  DrinksCategory(models.Model):
+    category_name=models.CharField(max_length=200)
+
+class Drinks(models.Model):
+    drink=models.CharField(max_length=200)
+    price=models.IntegerField()
+    category_id=models.ForeignKey(DrinksCategory,on_delete=models.PROTECT,default=None)
+
+
+
