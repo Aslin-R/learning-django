73101670d6907470ae7b471e8347a15a2618fd43
Aslin-R <aslinjoelr123@gmail.com>
2023-02-01 20:25:58 +0530

    updated one more change in models.py

--- django2/djangocoursera/myapp/models.py
+++ django2/djangocoursera/myapp/models.py
@@ -6,4 +6,4 @@ class MenuCategory(models.Model):
 class Menu(models.Model):
     menu_itm=models.CharField(max_length=100)
     price=models.IntegerField(null=False)
-    category_id=models.ForeignKey(MenuCategory,on_delete=models.PROTECT,default=None)
+    category_id=models.ForeignKey(MenuCategory,on_delete=models.PROTECT,default=None,related_name='category_name')
