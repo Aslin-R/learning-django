d903eed06b4f6d41e02e0567670e5bfb1a55b589
Aslin-R <aslinjoelr123@gmail.com>
2023-02-01 20:22:34 +0530

    Using foreign key on models

---
+++ django2/djangocoursera/myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class MenuCategory(models.Model):
+    menu_category_name=models.CharField(max_length=200)
+
+class Menu(models.Model):
+    menu_itm=models.CharField(max_length=100)
+    price=models.IntegerField(null=False)
+    category_id=models.ForeignKey(MenuCategory,on_delete=models.PROTECT,default=None)
