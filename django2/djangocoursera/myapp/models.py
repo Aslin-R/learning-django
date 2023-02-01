3832878094a41be15a19bfa83fc4bce9ee818f86
Aslin-R <aslinjoelr123@gmail.com>
2023-02-01 11:34:37 +0530

    maded migration in myapp

--- django2/djangocoursera/myapp/models.py
+++ django2/djangocoursera/myapp/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.db import models
 
 class Menu(models.Model):
-    name=models.CharField(max_length=100)
+    item_name=models.CharField(max_length=100)
     cuisine=models.CharField(max_length=100)
     price=models.IntegerField()
+    available=models.BooleanField(default=True)
+
+    
 
     def __str__(self):
         return self.name+' : '+self.cuisine
