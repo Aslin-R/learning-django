1b56c479971928e9426ed7dca83a3bd837007a7b
Aslin-R <aslinjoelr123@gmail.com>
2023-02-01 10:39:19 +0530

    introduction to models and migrations

--- django2/djangocoursera/djangocoursera/views.py
+++ django2/djangocoursera/djangocoursera/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 
 def handler404(request, exception):
     return HttpResponse("Hi Bro ::: 404 error : page not found!!!")  
