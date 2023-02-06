d7eef156997d822cdf24718815c61c8e2f05d6d1
Aslin R <aslinjoelr123@gmail.com>
2023-02-06 20:07:59 +0530

    removed some stuff

--- django2/djangocoursera/myapp/views.py
+++ django2/djangocoursera/myapp/views.py
@@ -2,37 +2,14 @@ from django.shortcuts import render
 from django.http import *
 # Create your views here.
 
-def info(request):
+def intro(request): 
     
-    path=request.path
-    scheme=request.scheme
-    method=request.method
-    address=request.META['REMOTE_ADDR']
-    user_agent=request.META['HTTP_USER_AGENT']
-    path_info=request.path_info
-    response=HttpResponse()
-    response.headers['Name']='Aslin R'
+    return HttpResponse("Welcome to Django") 
 
-    msg=f""" <br>
-    <br>Path:{path}
-    <br>Scheme:{scheme}
-    <br>Method:{method}
-    <br>Address:{address}
-    <br>User Agent:{user_agent}
-    <br>Path Info:{path_info}
-    <br>Response Header:{response.headers}
-    """
-    return HttpResponse(msg)
-  
 def qryview(request): 
     name = request.GET['name'] 
     id = request.GET['id'] 
     return HttpResponse("Name:{} UserID:{}".format(name, id)) 
-
-
-def showform(request): 
-    return render(request,'templates/form.html') 
-
 
 def drinks(request,drink_name):
     drink={
@@ -45,15 +22,6 @@ def drinks(request,drink_name):
 def menu(request,num):
     items=['Dosai','Puttu','Biriyani']
     return HttpResponse(f'<h2>{items[num]}</h2>')
-
-def home(request):
-    return HttpResponse('Welcome to Little Lemon!')
-
-def about(request):
-    return HttpResponse('About us')
-
-def book(request):
-    return HttpResponse('Make a booking')
 
 from .forms import ContactForm
 
