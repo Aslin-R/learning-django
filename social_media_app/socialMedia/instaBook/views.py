005ca99c2365f8bf7d0e0cdde7b55e4bd128bc23
aslin-r <aslinjoelr123@gmail.com>
2023-02-18 10:05:42 +0530

    worked on signin and logout

--- social_media_app/socialMedia/instaBook/views.py
+++ social_media_app/socialMedia/instaBook/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from . models import Profile
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
+@login_required(login_url='signin')
 def index(request):
-    print('Hello World')
     return render(request,'index.html')
 
 def signup(request):
@@ -33,7 +35,7 @@ def signup(request):
                 #profile object for new user
 
                 user_model=User.objects.get(username=username)
-                new_profile=Profile.objects.create(user=user_model,id_user=user)
+                new_profile=Profile.objects.create(user=user_model,id_user=user_model)
                 new_profile.save()
                 return redirect('signup')
 
@@ -45,3 +47,25 @@ def signup(request):
 
     else:    
         return render(request,'signup.html')
+    
+def signin(request):
+
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password'] 
+
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Credentials is Invalid')
+            return redirect('signin')
+    else:
+        return render(request,'signin.html')
+    
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
