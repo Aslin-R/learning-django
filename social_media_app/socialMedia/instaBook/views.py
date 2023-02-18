66daeac459e73ab66648f052f326603d562d07ca
aslin-r <aslinjoelr123@gmail.com>
2023-02-18 19:15:52 +0530

    worked on settings

--- social_media_app/socialMedia/instaBook/views.py
+++ social_media_app/socialMedia/instaBook/views.py
@@ -32,12 +32,17 @@ def signup(request):
                 user=User.objects.create_user(username=username,email=email,password=password,)
                 user.save()
 
+                #sign up user to redirect to settings
+                
+                user_login=auth.authenticate(username=username,password=password)
+                auth.login(request,user_login)
+
                 #profile object for new user
 
                 user_model=User.objects.get(username=username)
                 new_profile=Profile.objects.create(user=user_model,id_user=user_model)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
 
         else:
             messages.info(request,'Password Not Matching')
@@ -69,3 +74,32 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile=Profile.objects.get(user=request.user)
+
+    if request.method=="POST":
+        if request.FILES.get('image')==None:
+            image=user_profile.profileimg
+            bio=request.POST['bio']
+            location=request.POST['location']
+
+            user_profile.profileimg=image
+            user_profile.bio=bio
+            user_profile.location=location
+            user_profile.save()
+
+        if request.FILES.get('image') !=None:
+            image=request.FILES.get('image')
+            bio=request.POST['bio']
+            location=request.POST['location']
+
+            user_profile.profileimg=image
+            user_profile.bio=bio
+            user_profile.location=location
+            user_profile.save()
+
+        return redirect('settings')
+
+    return render(request,'setting.html',{'user_profile':user_profile})
