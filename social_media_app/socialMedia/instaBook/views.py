2b510682ac164e059489e1778754aab1a139f9d5
aslin-r <aslinjoelr123@gmail.com>
2023-02-19 22:43:48 +0530

    Completed upload option

--- social_media_app/socialMedia/instaBook/views.py
+++ social_media_app/socialMedia/instaBook/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
-from . models import Profile
+from . models import Profile,Post
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -110,7 +110,19 @@ def settings(request):
 
 @login_required(login_url='signin')
 def upload(request):
-    return HttpResponse('Uploaded')
+    if request.method=='POST':
+        user=request.user.username
+        image=request.FILES.get('image_upload')
+        caption=request.POST['caption']
+
+        new_post=Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+
+        return redirect('/')
+
+    else:
+        return redirect('/')
+    
 
 
 
